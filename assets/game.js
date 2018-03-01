b78679937c3fcdbeb2a3cb91e2f9756b5f2e1c33
brad ferrell <ferrellbh@gmail.com>
2018-02-28 19:18:55 -0500

    function tweak

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,5 @@
 //Generates random number between 19 & 120.
-var randNum = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-Math.floor(Math.random() * 119 + 2);
+var randNum = Math.floor(Math.random() * 119 + 2);
 //Print randNum to showRN card.
 $(".showRN").html(randNum);
 
